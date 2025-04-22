94001fcd7c7128d93f02057a655db7e430a8467a
Palo Silva <pds27@duke.edu>
2025-04-21 23:30:39 -0400

    updated song_play assembly

--- song_play.s
+++ song_play.s
@@ -1,48 +1,29 @@
 main:
-    addi $1, $0, 4096       # Address of input-mapped I/O (SW)
-    nop
-    nop
-    nop
-    lw   $2, 0($1)          # Load switch input into $2
-    addi $3, $0, 1          # Constant 1 for masking
+    addi $1, $0, 4096       # $1 = address to read switches
+    lw   $2, 0($1)          # $2 = switch value (SW[15:0])
+    addi $3, $0, 1          # $3 = mask = 0x1
+    addi $5, $0, 0          # $5 = song index = 0
 
 check_switches:
-    and  $4, $2, $3         # Check if current switch bit is set
-    nop
-    nop
-    nop
-    nop
-    beq  $4, $0, shift_next # If not set, check next
-    addi $5, $0, 0          # Song index (will be increased below)
-
-    # Calculate song start address = song index * 256
-    sll  $6, $5, 8          # $6 = base address of song in memory
-
-    # Set up loop counter for song length
-    addi $7, $0, 256        # 256 samples
-    addi $8, $0, 0          # Offset into song
+    and  $4, $2, $3         # $4 = result of masked switch
+    bne  $4, $0, play_song  # If bit is ON, jump to play_song
+    sll  $3, $3, 1          # shift mask left
+    addi $5, $5, 1          # increment song index
+    bne  $5, $16, check_switches  # Loop while index < 16
+    j done                  # None pressed, halt
 
 play_song:
-    add  $9, $6, $8         # Current song address = base + offset
-    lw   $10, 0($9)         # Load sample
-    addi $11, $0, 4097      # Audio output-mapped I/O address
-    sw   $10, 0($11)        # Write sample to audioOut
+    sll  $6, $5, 8          # $6 = song address offset = song# * 256
+    addi $7, $0, 256        # $7 = sample count (loop limit)
+    addi $8, $0, 0          # $8 = offset within song
+
+play_loop:
+    add  $9, $6, $8         # $9 = current sample address
+    lw   $10, 0($9)         # $10 = current sample
+    addi $11, $0, 4097      # $11 = audioOut I/O address
+    sw   $10, 0($11)        # send to audioOut
     addi $8, $8, 1          # offset++
-    nop
-    nop
-    nop
-    nop
-    bne  $8, $7, play_song  # Loop until all samples played
+    bne  $8, $7, play_loop  # loop until 256 samples played
 
 done:
-    j done                 # Halt (infinite loop)
-
-shift_next:
-    sll  $3, $3, 1          # Shift mask left
-    addi $5, $5, 1          # Song index++
-    nop
-    nop
-    nop
-    nop
-    blt  $5, 16, check_switches  # Max 16 switches
-    j done
+    j done                  # loop forever
